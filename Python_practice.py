833b91c9bc7876d6e1f8d5fe7365c824b86d094b
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-03-18 10:06:04 -0500

    Create Python_practice.py
    update

---
+++ Python_practice.py
@@ -0,0 +1,16 @@
+name = "Randy"
+country = "Russia"
+age = 33
+hourly_wage = 18
+satisfied = True
+daily_wage = hourly_wage * 8
+
+print("Hello " + name)
+
+print("You live in " + country)
+
+print(name + " is " + str(age) + " years old")
+
+print(name + " earns " + str(hourly_wage) + " per day")
+
+print(f"Are you satisfied with your current wage? {satisfied}")
