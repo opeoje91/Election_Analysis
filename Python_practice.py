e6fdd4fe9246690fd831a545836d6a2515622779
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-03-18 18:43:46 -0500

    Update Python_practice.py

--- Python_practice.py
+++ Python_practice.py
@@ -1,16 +1,29 @@
-name = "Randy"
-country = "Russia"
-age = 33
-hourly_wage = 18
-satisfied = True
-daily_wage = hourly_wage * 8
+#counties = ["Arapahoe","Denver","Jefferson"]
+#if counties[1] == 'Denver':
+#  print(counties[1])
+#counties = ["Arapahoe","Denver","Jefferson"]
+#if counties[3] != 'Jefferson':
+#  print(counties[2])
+##Membership and Logical Operators
+counties = ["Arapahoe", "Denver", "Jefferson"]
+#if "El Paso" in counties:
+#   print("El Paso is in the list of counties.")
+#else:
+#   print("El Paso is not in the lsit of counties.")
+#if "Arapahoe" in counties and "El Paso" in counties:
+#    print("Arapahoe and El Paso are in the list of counties.")
+#else:
+ #   print("Arapahoe or El Paso is not in the list of counties.")
+#if "Arapahoe" in counties or "El Paso" in counties:
+#   print("Arapahoe or El Paso is in the list of counties.")
+#else:
+#    print("Arapahoe or El Paso are not in the list of counties.")
+#if "Arapahoe" and "El Paso" not in counties:
+#    print("El Paso is not in the list of counties.")
+#else:
+#    print("Arapahoe and El Paso are not in the list of counties.")
+if "Arapahoe" in counties and "El Paso" not in counties:
+  print("Only Arapahoe is in the list of counties.")
+else:
+   print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
 
-print("Hello " + name)
-
-print("You live in " + country)
-
-print(name + " is " + str(age) + " years old")
-
-print(name + " earns " + str(hourly_wage) + " per day")
-
-print(f"Are you satisfied with your current wage? {satisfied}")
