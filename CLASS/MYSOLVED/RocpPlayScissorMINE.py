04abb1fc4a74a2b7b1b6ed49263468abb631a7ee
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-03-19 18:09:44 -0500

    Create RocpPlayScissorMINE.py

---
+++ CLASS/MYSOLVED/RocpPlayScissorMINE.py
@@ -0,0 +1,28 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\BootCampFiles\Election_Analysis\CLASS\Par_RockPaperScissors_MINE.py
+Let's Play Rock Paper Scissors!
+Make your Choice: (r)ock, (p)aper, (s)cissors? r
+You have won!
+>>> y
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    y
+NameError: name 'y' is not defined
+>>> 
+= RESTART: C:\BootCampFiles\Election_Analysis\CLASS\Par_RockPaperScissors_MINE.py
+Let's Play Rock Paper Scissors!
+Make your Choice: (r)ock, (p)aper, (s)cissors? p
+There is a draw
+>>> 
+= RESTART: C:\BootCampFiles\Election_Analysis\CLASS\Par_RockPaperScissors_MINE.py
+Let's Play Rock Paper Scissors!
+Make your Choice: (r)ock, (p)aper, (s)cissors? s
+There is a draw
+>>> 
+= RESTART: C:\BootCampFiles\Election_Analysis\CLASS\Par_RockPaperScissors_MINE.py
+Let's Play Rock Paper Scissors!
+Make your Choice: (r)ock, (p)aper, (s)cissors? p
+You have won!
+>>> 
