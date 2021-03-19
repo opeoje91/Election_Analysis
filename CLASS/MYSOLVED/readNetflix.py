3f38f81c3de7e785a610a7f0859500e39587258a
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-03-19 18:10:26 -0500

    Create readNetflix.py

---
+++ CLASS/MYSOLVED/readNetflix.py
@@ -0,0 +1,11 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:\BootCampFiles\Election_Analysis\CLASS\Stu_ReadNetflix.py =====
+What show or movie are you looking for? White Chicks
+White Chicks is rated PG-13 with a rating of82
+>>> 
+===== RESTART: C:\BootCampFiles\Election_Analysis\CLASS\Stu_ReadNetflix.py =====
+What show or movie are you looking for? White Chicks
+White Chicks is rated PG-13 with a rating of82
+>>> 
