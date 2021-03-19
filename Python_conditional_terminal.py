798301cfb8162538d2d987ac0f9640f69769f16e
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-03-19 18:09:10 -0500

    Create Python_conditional_terminal.py

---
+++ Python_conditional_terminal.py
@@ -0,0 +1,11 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/BootCampFiles/Election_Analysis/Python_conditionals.py ======
+0
+1
+2
+3
+4
+5
+>>> 
