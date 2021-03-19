72e8f50d403cb780c81322698cea43257e139123
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-03-19 18:09:06 -0500

    Create Python_Practice_Terminal.py

---
+++ Python_Practice_Terminal.py
@@ -0,0 +1,17 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/BootCampFiles/Election_Analysis/tuples.py ============
+>>> 
+============= RESTART: C:/BootCampFiles/Election_Analysis/tuples.py ============
+[{'county': 'Arapahoe', 'registered_voters': 422829}, {'county': 'El Paso', 'registered_voters': 461149}, {'county': 'Denver', 'registered_voters': 463353}, {'county': 'Jefferson', 'registered_voters': 432438}]
+>>> 
+============= RESTART: C:/BootCampFiles/Election_Analysis/tuples.py ============
+[{'county': 'El Paso', 'registered_voters': 461149}, {'county': 'Denver', 'registered_voters': 463353}, {'county': 'Jefferson', 'registered_voters': 432438}]
+>>> 
+============= RESTART: C:/BootCampFiles/Election_Analysis/tuples.py ============
+[{'county': 'El Paso', 'registered_voters': 461149}, {'county': 'Jefferson', 'registered_voters': 432438}, {'county': 'Denver', 'registered_voters': 463353}]
+>>> 
+============= RESTART: C:/BootCampFiles/Election_Analysis/tuples.py ============
+[{'county': 'El Paso', 'registered_voters': 461149}, {'county': 'Jefferson', 'registered_voters': 432438}, {'county': 'Denver', 'registered_voters': 463353}, {'county': 'Arapahoe', 'registered_voters': 422829}]
+>>> 
