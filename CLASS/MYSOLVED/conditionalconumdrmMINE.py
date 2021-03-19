5ec9f0148a151ee8a0c107e2131144dcb8ed3af9
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-03-19 18:10:50 -0500

    Create conditionalconumdrmMINE.py

---
+++ CLASS/MYSOLVED/conditionalconumdrmMINE.py
@@ -0,0 +1,10 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\BootCampFiles\Election_Analysis\CLASS\Evr_ConditionalConundrum.py
+oooo needs some work
+Question 2 works!
+GOT QUESTION 3!
+Dan is in group three
+Can ride bumper cars
+>>> 
