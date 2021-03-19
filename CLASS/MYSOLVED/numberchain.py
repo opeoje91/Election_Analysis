60593e245e1064e6ff7d716ab1fd6802cfebc336
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-03-19 18:10:47 -0500

    Create numberchain.py

---
+++ CLASS/MYSOLVED/numberchain.py
@@ -0,0 +1,49 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:\BootCampFiles\Election_Analysis\CLASS\Stu_NumberChain.py =====
+How many numbers?6
+0
+1
+2
+3
+4
+5
+Continue: (y)es or (n)o? y
+How many numbers?5
+0
+1
+2
+3
+4
+Continue: (y)es or (n)o? n
+>>> 
+===== RESTART: C:\BootCampFiles\Election_Analysis\CLASS\Stu_NumberChain.py =====
+How many numbers?7
+Traceback (most recent call last):
+  File "C:\BootCampFiles\Election_Analysis\CLASS\Stu_NumberChain.py", line 14, in <module>
+    Play_count += 1
+NameError: name 'Play_count' is not defined
+>>> 
+===== RESTART: C:\BootCampFiles\Election_Analysis\CLASS\Stu_NumberChain.py =====
+Traceback (most recent call last):
+  File "C:\BootCampFiles\Election_Analysis\CLASS\Stu_NumberChain.py", line 3, in <module>
+    p#lay_count = 0
+NameError: name 'p' is not defined
+>>> 
+===== RESTART: C:\BootCampFiles\Election_Analysis\CLASS\Stu_NumberChain.py =====
+How many numbers? 7
+0
+1
+2
+3
+4
+5
+6
+Continue: (y)es or (n)o? y
+How many numbers? 3
+0
+1
+2
+Continue: (y)es or (n)o? n
+>>> 
