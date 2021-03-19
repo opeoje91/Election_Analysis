1a23d7b560f4aa6d4779ca21fbd556ff93e869f5
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-03-19 18:10:34 -0500

    Create numberchainbonus.py

---
+++ CLASS/MYSOLVED/numberchainbonus.py
@@ -0,0 +1,47 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\BootCampFiles\UTA-VIRT-DATA-PT-02-2021-U-B\03-Lesson-Plans\03-Lessons\2\Activities\05-Stu_NumberChain\Unsolved\NumberChain_Unsolved.py
+How many numbers? 7
+0
+1
+2
+3
+4
+5
+6
+Continue the chain: (y)es or (n)o? y
+How many numbers? 5
+7
+8
+9
+10
+11
+Continue the chain: (y)es or (n)o? 
+>>> y
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    y
+NameError: name 'y' is not defined
+>>> 
+= RESTART: C:\BootCampFiles\UTA-VIRT-DATA-PT-02-2021-U-B\03-Lesson-Plans\03-Lessons\2\Activities\05-Stu_NumberChain\Unsolved\NumberChain_Unsolved.py
+How many numbers? 5
+0
+1
+2
+3
+4
+Continue the chain: (y)es or (n)o? y
+How many numbers? 5
+5
+6
+7
+8
+9
+Continue the chain: (y)es or (n)o? y
+How many numbers? 3
+10
+11
+12
+Continue the chain: (y)es or (n)o? n
+>>> 
