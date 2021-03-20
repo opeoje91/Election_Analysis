bea5469e65e6ee32a95fa01ebb2fe7071f59c464
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-03-19 23:50:02 -0500

    Adding the election audit code.

--- PyPoll_Terminal_Real.py
+++ PyPoll_Terminal_Real.py
@@ -1,6 +1,74 @@
 Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
-====== RESTART: C:/BootCampFiles/Election_Analysis/PyPoll_Console_Real.py ======
-['Ballot ID', 'County', 'Candidate']
+====== RESTART: C:\BootCampFiles\Election_Analysis\PyPoll_Console_Real.py ======
+{'Charles Casper Stockham': 1, 'Diana DeGette': 1, 'Raymon Anthony Doane': 1}
+>>> 
+====== RESTART: C:\BootCampFiles\Election_Analysis\PyPoll_Console_Real.py ======
+{'Charles Casper Stockham': 85213, 'Diana DeGette': 272892, 'Raymon Anthony Doane': 11606}
+>>> 
+====== RESTART: C:\BootCampFiles\Election_Analysis\PyPoll_Console_Real.py ======
+Charles Casper Stockham: received 23.04854332167558% of the vote.
+Diana DeGette: received 73.81224794501652% of the vote.
+Raymon Anthony Doane: received 3.1392087333079077% of the vote.
+>>> 
+====== RESTART: C:\BootCampFiles\Election_Analysis\PyPoll_Console_Real.py ======
+Charles Casper Stockham: received (23.04854332167558%.01f) of the vote.
+Diana DeGette: received (73.81224794501652%.01f) of the vote.
+Raymon Anthony Doane: received (3.1392087333079077%.01f) of the vote.
+>>> 
+====== RESTART: C:\BootCampFiles\Election_Analysis\PyPoll_Console_Real.py ======
+Charles Casper Stockham: received 23.0% of the vote.
+Diana DeGette: received 73.8% of the vote.
+Raymon Anthony Doane: received 3.1% of the vote.
+>>> 
+====== RESTART: C:\BootCampFiles\Election_Analysis\PyPoll_Console_Real.py ======
+>>> 
+====== RESTART: C:\BootCampFiles\Election_Analysis\PyPoll_Console_Real.py ======
+Charles Casper Stockham: 23.0% (85,213)
+
+Diana DeGette: 73.8% (272,892)
+
+Raymon Anthony Doane: 3.1% (11,606)
+
+>>> 
+====== RESTART: C:\BootCampFiles\Election_Analysis\PyPoll_Console_Real.py ======
+Charles Casper Stockham: 23.0% (85,213)
+
+-------------------------
+Winner: Charles Casper Stockham
+Winning Vote Count: 85,213
+Winning Percentage: 23.0%
+-------------------------
+
+Diana DeGette: 73.8% (272,892)
+
+-------------------------
+Winner: Diana DeGette
+Winning Vote Count: 272,892
+Winning Percentage: 73.8%
+-------------------------
+
+Raymon Anthony Doane: 3.1% (11,606)
+
+-------------------------
+Winner: Diana DeGette
+Winning Vote Count: 272,892
+Winning Percentage: 73.8%
+-------------------------
+
+>>> 
+====== RESTART: C:\BootCampFiles\Election_Analysis\PyPoll_Console_Real.py ======
+Charles Casper Stockham: 23.0% (85,213)
+
+Diana DeGette: 73.8% (272,892)
+
+Raymon Anthony Doane: 3.1% (11,606)
+
+-------------------------
+Winner: Diana DeGette
+Winning Vote Count: 272,892
+Winning Percentage: 73.8%
+-------------------------
+
 >>> 
