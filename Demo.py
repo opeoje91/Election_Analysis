cb8714badff885e6dec7fc7f8ec58f652dec8d68
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-03-23 17:11:26 -0500

    Create Demo.py

---
+++ Demo.py
@@ -0,0 +1,13 @@
+# Description of file
+
+# Imports
+import os
+
+# Decription of function
+def MyFirstFunction():
+    print("Hello First Function")
+
+# Description of function
+def MySecondFunction():
+    print("Hello Second Function")  
+
